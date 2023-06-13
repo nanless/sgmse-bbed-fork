a25b93480436d263612e7050cc9bef7827f336aa
nanless <francis7999@outlook.com>
2023-06-13 16:14:53 +0800

    add dit

--- sgmse/backbones/__init__.py
+++ sgmse/backbones/__init__.py
@@ -1,5 +1,6 @@
 from .shared import BackboneRegistry
 from .ncsnpp import NCSNpp
 from .dcunet import DCUNet
+from .dit import DiT
 
-__all__ = ['BackboneRegistry', 'NCSNpp', 'DCUNet']
+__all__ = ['BackboneRegistry', 'NCSNpp', 'DCUNet', 'DiT']
